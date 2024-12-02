48e3019af5516a75b08012eaa1807238870a500f
juma886 <wandirajuma22@gmail.com>
2024-12-03 00:32:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const openBtn=document.querySelector('#open-modal');
+// const closebtn=docemunt.querySelector('#close-modal');
+
+openBtn.addEventListener('click',()=>dialog.showModal());
+closebtn.addEventListener('click', ()=> dialog.close());
+
